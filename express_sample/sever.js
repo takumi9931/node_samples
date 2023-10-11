829c30197391a451ff42db6136c7a92d64ad9f3e
takumi <220022@yse-c.net>
2023-10-11 12:16:03 +0900

    first commit

--- express_sample/sever.js
+++ express_sample/sever.js
@@ -2,6 +2,8 @@
 const express = require('express')
 // dotenvモジュール読み込み
 const dotenv = require('dotenv')
+// routerモジュール読み込み
+const routes = require('./routes')
 
 // dotenvの設定読み込み
 dotenv.config()
@@ -18,34 +20,8 @@ app.use(express.static(__dirname + '/public'))
 // URLエンコード
 app.use(express.urlencoded({ extended: true }))
 
-// GETリクエストの処理
-app.get('/', (req, res) => {
-    // リクエストの処理
-    console.log(req.body)
-    console.log(req.url)
-    console.log(req.query)
-
-    // レスポンスの処理
-    res.send('Hello!!!!!!')
-})
-
-app.get('/profile', (req, res) => {
-    res.send('プロフィール')
-})
-
-// POSTリクエスト
-app.post('/auth', (req, res) => {
-    var loginName = req.body.login_name
-    var password = req.body.password
-    console.log(loginName, password)
-
-    var message = "ログイン失敗"
-    if (loginName == process.env.LOGIN_NAME
-        && password == process.env.PASSWORD) {
-            message = "ログイン成功"
-    }
-    res.send(message)
-})
+// ルーティングを有効
+app.use(routes)
 
 //　サーバ停止: 起動中のターミナルで Ctrl + C
 // サーバ待機（Listen）
