41be863dc6980444568cfd252da57d67b5efae48
takumi <220022@yse-c.net>
2023-10-11 12:09:40 +0900

    first commit

--- express_sample/sever.js
+++ express_sample/sever.js
@@ -11,6 +11,13 @@ const PORT = process.env.PORT
 // サーバ作成
 const app = express()
 
+// ミドルウェアの設定
+// publicフォルダを静的コンテンツのフォルダに設定
+app.use(express.static(__dirname + '/public'))
+
+// URLエンコード
+app.use(express.urlencoded({ extended: true }))
+
 // GETリクエストの処理
 app.get('/', (req, res) => {
     // リクエストの処理
@@ -20,6 +27,24 @@ app.get('/', (req, res) => {
 
     // レスポンスの処理
     res.send('Hello!!!!!!')
+})
+
+app.get('/profile', (req, res) => {
+    res.send('プロフィール')
+})
+
+// POSTリクエスト
+app.post('/auth', (req, res) => {
+    var loginName = req.body.login_name
+    var password = req.body.password
+    console.log(loginName, password)
+
+    var message = "ログイン失敗"
+    if (loginName == process.env.LOGIN_NAME
+        && password == process.env.PASSWORD) {
+            message = "ログイン成功"
+    }
+    res.send(message)
 })
 
 //　サーバ停止: 起動中のターミナルで Ctrl + C
